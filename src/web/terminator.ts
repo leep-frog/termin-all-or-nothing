6a22dd5710d6c39e3ff7f5dfe5cd9c21cef1bc9c
leep-frog <gleeper49@gmail.com>
2023-01-13 09:36:50 -0500

    Add auto-close en/disable commands and execute wrapper

--- src/web/terminator.ts
+++ src/web/terminator.ts
@@ -1,16 +1,20 @@
 import * as vscode from 'vscode';
 
+function extensionCommand(subCommand : string) : string {
+  return `termin-all-or-nothing.${subCommand}`;
+}
+
 const unsetCommands : string[] = [
   // Don't override this because we call it ourselves.
   // "workbench.action.toggleMaximizedPanel",
 ];
 const openCommands = [
   "terminal.focus",
-  "termin-all-or-nothing.openPanel",
+  extensionCommand("openPanel"),
   "workbench.action.terminal.toggleTerminal",
 ];
 const closeCommands = [
-  "termin-all-or-nothing.closePanel",
+  extensionCommand("closePanel"),
 ];
 
 /*
@@ -25,6 +29,7 @@ Tests:
 - switch panel tab to output tab (should not close panel)
 - Reload window (starting in editor), openPanel to output tab (should not close panel)
 - Reload window (starting in panel), switch focus to and from output tab (should not close panel)
+- Test running go.test.package commmand and groog.message.info (with args) wrapped by execute
 */
 
 function rangeToString(range : vscode.Range) : string {
@@ -43,22 +48,29 @@ function differenceOutputOnly(setA : Set<string>, setB : Set<string>) : boolean 
 export class Terminator {
 
   private togglingPanel : boolean;
+  private autoClosingEnabled : boolean;
 
   // The previous editor that wasn't an output editor.
-  private previouslyActiveNonOutputEditor : vscode.Uri | undefined;
+  /*private previouslyActiveNonOutputEditor : vscode.Uri | undefined;
   private previouslyActiveEditorWasOutput : boolean;
 
-  private previouslyVisibleEditors : Set<string>;
+  private previouslyVisibleEditors : Set<string>;*/
 
   constructor() {
     this.togglingPanel = false;
+    this.autoClosingEnabled = true;
 
-    this.previouslyActiveNonOutputEditor = (vscode.window.activeTextEditor && !isOutputUri(vscode.window.activeTextEditor.document.uri)) ? vscode.window.activeTextEditor.document.uri : undefined;
+    /*this.previouslyActiveNonOutputEditor = (vscode.window.activeTextEditor && !isOutputUri(vscode.window.activeTextEditor.document.uri)) ? vscode.window.activeTextEditor.document.uri : undefined;
     this.previouslyActiveEditorWasOutput = false;
-    this.previouslyVisibleEditors = new Set<string>();
+    this.previouslyVisibleEditors = new Set<string>();*/
   }
 
   activate(context: vscode.ExtensionContext) {
+
+    this.registerCommand(context, extensionCommand("autoClosePanel.enable"), () => this.setAutoClose(true));
+    this.registerCommand(context, extensionCommand("autoClosePanel.disable"), () => this.setAutoClose(false));
+
+    this.registerCommand(context, extensionCommand("execute"), (args : ExecuteArgs) => this.execute(args));
 
     // This only activates when a terminal is created, focus is changed from one terminal
     // to another terminal, or all terminals are terminated (aka permanently closed).
@@ -68,13 +80,16 @@ export class Terminator {
     // This event only triggers when a new terminal is created.
     // context.subscriptions.push(vscode.window.onDidOpenTerminal((activeEditor) => {}));
 
-    context.subscriptions.push(vscode.window.onDidChangeTextEditorVisibleRanges((event) => {
-      if (event.textEditor.document.uri.scheme !== "output" && event.visibleRanges.length) {
-        this.closePanel("TextEditorVisibleRange");
+    // This is activated when a file is opened (via File Explorer or ctrl+click in terminal) from
+    // the panel view. Those actions cause a half-open page and half-open panel.
+    // This event notices that the visible ranges of the files changed and closes the terminal.
+    this.register(context, vscode.window.onDidChangeTextEditorVisibleRanges((event) => {
+      if (!isOutputUri(event.textEditor.document.uri) && event.visibleRanges.length) {
+        this.closePanel(false, "TextEditorVisibleRange");
       }
     }));
 
-    context.subscriptions.push(vscode.window.onDidChangeVisibleTextEditors((visibleEditors) => {
+    /*this.register(context, vscode.window.onDidChangeVisibleTextEditors((visibleEditors) => {
       const newlyVisibleEditors = new Set(visibleEditors.map(ve => ve.document.uri.toString()));
 
       if (!visibleEditors.length) {
@@ -82,7 +97,7 @@ export class Terminator {
         return;
       }
 
-      if (visibleEditors.filter(ve => ve.document.uri.scheme !== "output").length === 0) {
+      if (visibleEditors.filter(ve => isOutputUri(ve.document.uri)).length === 0) {
         this.previouslyVisibleEditors = newlyVisibleEditors;
         return;
       }
@@ -101,7 +116,7 @@ export class Terminator {
     // This only triggers when a file is closed or focus changes. This won't
     // trigger when the terminal is open (because the active editor behind the
     // terminal is still the same).
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((activeEditor) => {
+    this.register(context, vscode.window.onDidChangeActiveTextEditor((activeEditor) => {
       if (!activeEditor) {
         return;
       }
@@ -124,53 +139,50 @@ export class Terminator {
 
       this.previouslyActiveNonOutputEditor = activeEditor.document.uri;
       this.previouslyActiveEditorWasOutput = false;
-    }));
+    }));*/
 
     // I don't use notebooks, but I'm assuming this is useful for those who do.
-    context.subscriptions.push(vscode.window.onDidChangeNotebookEditorVisibleRanges((event) => {
-      // Only close the panel if there are some visible editors.
-      if (event.visibleRanges.length > 0) {
-        this.closePanel("NB1");
+    this.register(context, vscode.window.onDidChangeNotebookEditorVisibleRanges((event) => {
+      if (!isOutputUri(event.notebookEditor.notebook.uri) && event.visibleRanges.length) {
+        this.closePanel(false, "NotebookEditorVisibleRange");
       }
     }));
-    context.subscriptions.push(vscode.window.onDidChangeVisibleNotebookEditors((visibleEditors) => {
+
+    /*this.register(context, vscode.window.onDidChangeVisibleNotebookEditors((visibleEditors) => {
       if (visibleEditors.length > 0) {
         this.closePanel("NB2");
       }
     }));
-    context.subscriptions.push(vscode.window.onDidChangeActiveNotebookEditor((activeEditor) => {
+    this.register(context, vscode.window.onDidChangeActiveNotebookEditor((activeEditor) => {
       if (activeEditor) {
         this.closePanel("NB3");
       }
-    }));
+    }));*/
 
     // Command registrations
     for (const cmd of unsetCommands) {
-      context.subscriptions.push(vscode.commands.registerCommand(cmd, () => {
-        vscode.window.showErrorMessage(`The command "${cmd}" has been unset by the termin-all-or-nothing extension`);
-      }));
+      this.registerCommand(context, cmd, () => vscode.window.showErrorMessage(`The command "${cmd}" has been unset by the termin-all-or-nothing extension`));
     }
 
     for (const cmd of openCommands) {
-      context.subscriptions.push(vscode.commands.registerCommand(cmd, () => {
-        this.openPanel();
-      }));
+      this.registerCommand(context, cmd, () => this.openPanel());
     }
 
     for (const cmd of closeCommands) {
-      context.subscriptions.push(vscode.commands.registerCommand(cmd, () => {
-        this.closePanel("CMD:" + cmd);
-      }));
+      this.registerCommand(context, cmd, () => this.closePanel(true, "CMD:" + cmd));
     }
   }
 
-  async closePanel(id : string) : Promise<void> {
-    if (this.togglingPanel) {
-      return;
-    }
-    this.togglingPanel = true;
-    await vscode.commands.executeCommand("workbench.action.closePanel");
-    this.togglingPanel = false;
+  register(context: vscode.ExtensionContext, ...sub : { dispose(): any }[]) {
+    context.subscriptions.push(...sub);
+  }
+
+  registerCommand(context: vscode.ExtensionContext, commandName: string, callback: (...args: any[]) => Thenable<any>) {
+    this.register(context, vscode.commands.registerCommand(commandName, callback));
+  }
+
+  async setAutoClose(value : boolean) : Promise<void> {
+    this.autoClosingEnabled = value;
   }
 
   async openPanel() : Promise<void> {
@@ -181,4 +193,30 @@ export class Terminator {
     await vscode.commands.executeCommand("workbench.action.toggleMaximizedPanel");
     this.togglingPanel = false;
   }
+
+  async closePanel(userInitiated : boolean, id : string) : Promise<void> {
+    if (this.togglingPanel) {
+      return;
+    }
+    if (userInitiated || this.autoClosingEnabled) {
+      this.togglingPanel = true;
+      await vscode.commands.executeCommand("workbench.action.closePanel");
+      this.togglingPanel = false;
+    }
+  }
+
+  async execute(args : ExecuteArgs) {
+    const oldValue : boolean = this.autoClosingEnabled;
+    this.autoClosingEnabled = !!args.autoCloseEnabled;
+
+    await vscode.commands.executeCommand(args.command, args.args);
+
+    this.autoClosingEnabled = oldValue;
+  }
+}
+
+export interface ExecuteArgs {
+  autoCloseEnabled: boolean | undefined;
+  command: string;
+  args: any;
 }
